abb7532945b1ad8f2af00611a05512e42b81709d
jaggywaggy <cjaggard2@gmail.com>
2024-05-22 05:27:51 +1200

    Clean up

--- init.lua
+++ init.lua
@@ -1,4 +1,12 @@
+-- Set up the Lua require path to include your config directory
+local config_path = vim.fn.stdpath('config') .. '/lua/?.lua;' .. vim.fn.stdpath('config') .. '/lua/?/init.lua;'
+package.path = config_path .. package.path
+
+-- Now you can require your modules normally
 require('config')
 require('keymaps')
-require('after/treesitter')
-require('plugins/packer')
+require('plugins.packer')
+
+-- For the `after/treesitter` which seems like a specific setup, ensure it's a module or adjust accordingly
+require('after.treesitter')
+
